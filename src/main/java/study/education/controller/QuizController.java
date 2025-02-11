ed7f5c6e319f28aca434340ed7bc9a234869d3e8
dlxla4820 <gorhfqhk@gmail.com>
2025-02-11 17:47:19 +0900

    feat : get 추가

--- src/main/java/study/education/controller/QuizController.java
+++ src/main/java/study/education/controller/QuizController.java
@@ -4,9 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import study.education.dto.QuizRequestDto;
 import study.education.dto.ResponseDto;
 import study.education.service.QuizService;
@@ -21,6 +20,14 @@ public class QuizController {
     public ResponseEntity<?> saveQuiz(@RequestBody QuizRequestDto requestBody) {
         ResponseDto<?> response = quizService.saveQuiz(requestBody);
         return new ResponseEntity<>(response.getData(), HttpStatus.valueOf(response.getCode()));
+    }
+
+    @GetMapping("/quiz/{quizId}")
+    public ModelAndView getQuiz(@PathVariable int quizId) {
+        ModelAndView mv = new ModelAndView();
+        //mv.addObject("quizId", quizId);
+        mv.setViewName("quiz");
+        return mv;
     }
 
 }
